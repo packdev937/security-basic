893cb0512306e3341b4d4d9cffed1b2ed41e428d
gundorit <packdev937@gmail.com>
2023-07-08 23:26:44 +0900

    feat : User Entity 생성

---
+++ src/main/java/com/packdev937/securitybasic/entity/User.java
@@ -0,0 +1,42 @@
+package com.packdev937.securitybasic.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    private String provider;
+    private String providerId;
+    @CreationTimestamp
+    private Timestamp createdTime;
+
+    @Builder
+    public User(String username, String password, String email, String role, String provider, String providerId, Timestamp createdTime) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.provider = provider;
+        this.providerId = providerId;
+        this.createdTime = createdTime;
+    }
+}
