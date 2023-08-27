5ab5d3917c2945c8bd8ddd2f259d188227d7d8ac
gundorit <packdev937@gmail.com>
2023-08-27 15:53:05 +0900

    fix : /** to /*

--- src/main/java/com/packdev937/securitybasic/config/FilterConfig.java
+++ src/main/java/com/packdev937/securitybasic/config/FilterConfig.java
@@ -11,7 +11,7 @@ public class FilterConfig {
     public FilterRegistrationBean<MyFilter1> filter1(){
         FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
         // 모든 요청에서 다
-        bean.addUrlPatterns("/**");
+        bean.addUrlPatterns("/*");
         // 우선 순위를 설정 (0이 가장 높음)
         bean.setOrder(0);
         return bean;
