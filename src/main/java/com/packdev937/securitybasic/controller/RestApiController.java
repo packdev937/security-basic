1befa9a8ebfe9d5a694a79e65616850296728bdd
gundorit <packdev937@gmail.com>
2023-08-27 20:33:19 +0900

    feat: 회원가입 메서드 join() 추가

--- src/main/java/com/packdev937/securitybasic/controller/RestApiController.java
+++ src/main/java/com/packdev937/securitybasic/controller/RestApiController.java
@@ -1,5 +1,31 @@
 package com.packdev937.securitybasic.controller;
 
+import com.packdev937.securitybasic.entity.User;
+import com.packdev937.securitybasic.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class RestApiController {
 
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/home")
+    public String home() {
+        return "<h1>Home</h1>";
+    }
+
+    @PostMapping("/join")
+    public String join(@RequestBody User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+        return "회원 가입 완료";
+    }
 }
